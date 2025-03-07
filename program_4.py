ea17d371456479de0bec49b101b31dccefb77dc0
MichaelStoll-unwsp <mastoll@students.unwsp.edu>
2025-03-07 16:04:16 -0600

    Add files via upload

--- program_4.py
+++ program_4.py
@@ -1,10 +1,19 @@
-# Program #4: Coordinates
-# Write a distance function that will take two 3-dimensional coordinates (as input) 
-# and will return (as output) the distance between those points in space.  
-# The 3-dimensional coordinates must be stored as tuples.
+#title:coordinates
+#author: michael stoll
+#date: 3/6/2025
+import math
 
-# Now write a mainline that has the user enter the two tuples.  
-# The mainline calls the distance function and stores the distance in a variable.  The mainline then displays the distance.  
-# Also include exception handling to deal with faulty input.
-# The distance between two points (x1,y1,z1) and (x2, y2, z2) is 
-#    given by:   sqrt ((x2-x1)^2 + (y2 - y1)^2 + (z1 - z2)^2) 
+def coordinates():
+    coordinate_set1 = (x1,y1,z1)
+    coordinate_set2 = (x2,y2,z2)
+    print(coordinate_set1, coordinate_set2)
+
+x1 = int(input('Enter first x coordinate:'))
+y1 = int(input('Enter first y coordinate:'))
+z1 = int(input('Enter first z coordinate:'))
+x2 = int(input('Enter second x coordinate:'))
+y2 = int(input('Enter second y coordinate:'))
+z2 = int(input('Enter second z coordinate:'))
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z1 - z2)**2)
+coordinates()
+print(f'The distance between the two coordinates is {distance} units')
